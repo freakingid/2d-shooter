40f76879f9b374f8ab0f9aeccbef4ef595eda0a4
Paul Kaiser <freakingid@github.com>
2017-03-16 22:49:02 -0500

    Destroying the target

---
+++ Assets/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Destroy bullet after 10 seconds, no matter what
+		// Forget that, how about 3 seconds?
+		Destroy(gameObject, 3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Items colliding with bullet?
+	void OnCollisionEnter2D(Collision2D coll) {
+		// Is it tagged "target"?
+		if(coll.gameObject.tag == "Target" ) {
+			// Destroy the thing the bullet hit
+			Destroy (coll.gameObject);
+			// Destroy the bullet itself
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/MovingTarget.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTarget : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().velocity = Vector2.down * 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
