82274515f50a5810c56c8d93859ee2e427b90921
Paul Kaiser <freakingid@github.com>
2017-03-16 22:28:04 -0500

    Shooting missiles

--- Assets/Scripts/MovePlayer2.cs
+++ Assets/Scripts/MovePlayer2.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovePlayer2 : MonoBehaviour {
+public class MovePlayer2 : MonoBehaviour
+{
+
+	public GameObject bullet;
+
 	// Use this for initialization
 	void Start ()
 	{
@@ -12,6 +16,7 @@ public class MovePlayer2 : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
+		// Player movement via arrow keys
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			gameObject.transform.Translate (Vector3.left * 0.1f);
 		}
@@ -23,6 +28,14 @@ public class MovePlayer2 : MonoBehaviour {
 		}
 		if (Input.GetKey (KeyCode.DownArrow)) {
 			gameObject.transform.Translate (Vector3.down * 0.1f);
+		}
+
+		// Player firing bullet via spacebar
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			// b is bullet; Adding GameObject with bullet sprite, Player.positon + offset up, WTF is Q.id?
+			GameObject b = (GameObject)(Instantiate (bullet, transform.position + transform.up * 1.5f, Quaternion.identity));
+			// Propel the bullet in the up direction. Is this relevant to rotation of the Player?
+			b.GetComponent<Rigidbody2D> ().AddForce (transform.up * 1000);
 		}
 	}
 }
