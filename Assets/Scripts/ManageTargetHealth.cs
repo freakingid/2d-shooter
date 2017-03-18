6ce503ebd6be456bfe3c968e6453fa0bd4b087e7
Paul Kaiser <freakingid@github.com>
2017-03-17 23:07:13 -0500

    Challenge 2

--- Assets/Scripts/ManageTargetHealth.cs
+++ Assets/Scripts/ManageTargetHealth.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 public class ManageTargetHealth : MonoBehaviour {
 	public int health, type;
 	public static int TARGET_BOULDER=0;
+	public static int TARGET_BOULDER_MEDIUM=1;
+	public static int TARGET_BOULDER_LARGE=2;
 
 	// Use this for initialization
 	void Start () {
 		// Set health based on the type of target
-		if (type == TARGET_BOULDER) {
+		switch (type) {
+		case 0:
 			health = 20;
+			gameObject.GetComponent<Renderer> ().material.color = Color.red;
+			break;
+		case 1:
+			health = 30;
+			gameObject.GetComponent<Renderer> ().material.color = Color.green;
+			break;
+		case 2:
+			health = 40;
+			gameObject.GetComponent<Renderer> ().material.color = Color.blue;
+			break;
+		default:
+			health = 20;
+			gameObject.GetComponent<Renderer> ().material.color = Color.white;
+			break;
 		}
 	}
 	
--- Assets/Scripts/SpawnMovingTargets.cs
+++ Assets/Scripts/SpawnMovingTargets.cs
@@ -7,6 +7,7 @@ public class SpawnMovingTargets : MonoBehaviour
 
 	float timer = 0;
 	public GameObject newObject;
+	private int newBoulderType;
 
 	// Use this for initialization
 	void Start ()
@@ -26,10 +27,11 @@ public class SpawnMovingTargets : MonoBehaviour
 		// Spawn a new target every 2 seconds
 		if (timer >= 2) {
 			GameObject t = (GameObject)(Instantiate (newObject, newPosition, Quaternion.identity));
-			// This works, but do we have to do SpriteRenderer?
-			// t.GetComponent<SpriteRenderer> ().color = Color.red;
-			t.GetComponent<Renderer> ().material.color = Color.red;
-
+			// Randomly decide what boulder type
+			newBoulderType = Random.Range (0, 3);
+			// Set the boulder type accordingly; This should also set color when it sets the health amount;
+			t.GetComponent<ManageTargetHealth> ().type = newBoulderType;
+			// Start over for creating new target
 			timer = 0;
 		}
 	}
